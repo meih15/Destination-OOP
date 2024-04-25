03920da302ed5233d56b3af11fbcd025d3bcbd84
Mei Huang <meihuang8328@gmail.com>
2024-04-25 15:55:51 -0700

    plane class tests completed

---
+++ __tests__/classes/Plane.test.js
@@ -0,0 +1,40 @@
+const Plane = require('../../classes/Plane');
+const Person = require('../../classes/Person')
+
+describe("Plane Class Tests", () => {
+    let plane 
+
+    beforeEach(() => {
+        plane = new Plane("JetBlue", "OriginAirportCode", "Japan")
+    })
+
+    test("Constructor should properly initialize properties", () => {
+        expect(plane.company).toBe("JetBlue");
+        expect(plane.origin).toBe("OriginAirportCode");
+        expect(plane.destination).toBe("Japan");
+        expect(plane.passengers).toEqual([]);
+    });
+
+    test("Properties should have the correct Data Type", () => {
+        expect(typeof plane.company).toBe("string")
+        expect(typeof plane.origin).toBe("string")
+        expect(typeof plane.destination).toBe("string")
+        expect(Array.isArray(plane.passengers)).toBe(true);
+    })
+
+
+    test("addPassenger() adds a passenger to the 'passengers' array", () => {
+        let passenger = new Person("Tyler", "Japan");
+        plane.addPassenger(passenger);
+        expect(plane.passengers).toEqual([passenger]);
+    });
+
+    test("getPassengers() returns the array of 'passengers'", () => {
+        let passenger1 = new Person("Bijan", "Chicago");
+        let passenger2 = new Person("Ben", "Las Vegas");
+        plane.addPassenger(passenger1);
+        plane.addPassenger(passenger2);
+        const passengers = plane.getPassengers();
+        expect(passengers).toEqual([passenger1, passenger2]);
+    });
+})

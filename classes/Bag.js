245269e3a637ef10a6fd7ec00417f7a70bb51040
Mei Huang <meihuang8328@gmail.com>
2024-04-25 17:07:02 -0700

    finished bag class and passed tests

--- classes/Bag.js
+++ classes/Bag.js
@@ -1,6 +1,18 @@
 class Bag {
 
+    constructor(weight, id) {
+        this.weight = weight;
+        this.id = id;
+        this.owner = null;
+    }
 
+    getOwner() {
+        return this.owner;
+    }
+
+    assignOwner(person) {
+        return this.owner = person
+    }
 }
 
 

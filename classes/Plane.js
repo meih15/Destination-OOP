12874b54ba26a2692a470429f4e7e961be2f2868
Mei Huang <meihuang8328@gmail.com>
2024-04-25 17:13:28 -0700

    finished plane class and passed tests

--- classes/Plane.js
+++ classes/Plane.js
@@ -1,6 +1,19 @@
 class Plane {
 
+    constructor(company, origin, destination) {
+        this.company = company;
+        this.origin = origin;
+        this.destination = destination
+        this.passengers = [];
+    }
 
+    addPassenger(passenger) {
+        this.passengers.push(passenger)
+    }
+
+    getPassengers(){
+        return this.passengers
+    }
 }
 
 

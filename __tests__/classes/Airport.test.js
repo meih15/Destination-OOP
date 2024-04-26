60a1810d08ada7490f501c987c56ab2242a215ae
Mei Huang <meihuang8328@gmail.com>
2024-04-25 17:04:17 -0700

    airport class tests completed

---
+++ __tests__/classes/Airport.test.js
@@ -0,0 +1,40 @@
+const Airport = require('../../classes/Airport');
+const Plane = require('../../classes/Plane')
+
+describe("Airport Class Tests", () => {
+    let airport
+
+    beforeEach(() => {
+        airport = new Aiport("name", "airportCode")
+    })
+
+    test("Constructor should properly initialize properties", () => {
+        expect(airport.name).toBe("name");
+        expect(airport.airportCode).toBe("airportCode");
+        expect(airport.planes).toEqual([]);
+    });
+
+
+    test("Properties should have the correct Data Type", () => {
+        expect(typeof airport.name).toBe("string")
+        expect(typeof airport.airportCode).toBe("string")
+        expect(Array.isArray(airport.planes)).toBe(true);
+    });
+
+
+    test("addPlaner() adds a plane to the 'planes' array", () => {
+        let plane = new Plane("JetBlue", "OriginAirportCode", "Japan") 
+        plane.addPlane(plane);
+        expect(airport.planes).toEqual([plane]);
+    });
+
+    test("getPlanes() returns the array of 'planes'", () => {
+        let plane1 = new Plane("JetBlue", "OriginAirportCode", "Japan");
+        let plane2 = new Plane("Delta", "OriginAirportCode1", "Banff");
+        airport.addPlane(plane1);
+        airport.addPlane(plane2);
+        const planes = airport.getPlanes();
+        expect(planes).toEqual([plane1, plane22]);
+    });
+
+})
